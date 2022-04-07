484ab9f8998cd6fe531915e3d5f7532bd14dcfcc
LISHA KARAR <lishakrr@gmail.com>
2022-04-07 16:16:35 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT
+const db = require('./config/mongoose.js');
+const todoListModel = require('./model/todoListSchema.js');
+
+// express app
+const app = express();
+
+// connecting database
+db();
+
+// set template engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+// serving static files
+app.use(express.static('public'));
+
+// middleware
+app.use(express.urlencoded({extended: false}));
+
+// routing
+app.use('/',require('./routes/home.js'));
+
+
+
+
+
+// server listening
+app.listen(port,(err)=>{
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`Server listening at http://localhost:${port}`);
+});
